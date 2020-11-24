50b77ecf47582fd82d9bb26043226171fa4831c9
Cody Nguyen <cody@simicart.com>
2020-11-24 17:24:48 +0700

    update package path

--- src/componentOverrideMapping.js
+++ src/componentOverrideMapping.js
@@ -3,5 +3,5 @@
  * example: [`@magento/venia-ui/lib/components/Main/main.js`]: './lib/components/Main/main.js'
  */
 module.exports = componentOverride = {
-    [`@magento/venia-ui/lib/components/ProductFullDetail/productFullDetail.js`]:'@simicart/addsizechart/src/override/productFullDetail.js',
+    [`@magento/venia-ui/lib/components/ProductFullDetail/productFullDetail.js`]:'@simicart/sizechart/src/override/productFullDetail.js',
 };
